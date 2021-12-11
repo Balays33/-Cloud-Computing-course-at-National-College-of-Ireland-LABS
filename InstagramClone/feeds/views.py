5b9ae654320a669f2b3f49eddaeb98a91bf86b84
EC2 Default User <ec2-user@ip-172-31-66-113.ec2.internal>
2021-12-11 19:24:50 +0000

    version 2

--- InstagramClone/feeds/views.py
+++ InstagramClone/feeds/views.py
@@ -32,7 +32,7 @@ def index(request):
     #print(timeGet())
     context= {}
     context['val'] = timeGet()
-    #print(context)   
+    #print(context['val'])   
     
     url =  'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=92757f89a5760a1310e7b5657fff90d2'
     
@@ -49,63 +49,8 @@ def index(request):
     
     context['city_weather'] = city_weather
     #print(context['city_weather'])
-    
-
-    
     feeds_database = Feed.objects.all()
     
-    """
-    for weaterCity in feeds_database:
-        #weatherF = feeds_database.get(titleF='telex')
-        feeds_database.weatherF.weaterCity = "Balazs"
-        print(feeds_database.weatherF)
-     
-    """   
-    
-    t = feeds_database.get(titleF='telex')
-    t.weatherF = 999  # change field
-    t.save() # this will update only
-    """
-    for each in Feed.objects.all():
-        tr = feeds_database.all()
-        tr.weatherF =333
-        tr.save()
-        #test = each.weatherF
-        #each.weatherF.save()
-        print(each.weatherF)
-       
-    for updateWeatherDATA in feeds_database:
-        updateW = feeds_database.updateWeatherDATA
-        updateW.weatherF = 22
-        updateW.save()
-        
-    myQuery = Feed.objects.all()
-    for item in myQuery:
-        item.weatherF = 55
-        item.save(update_fields=['weatherF'])
-    """ 
-    entry_list = list(Feed.objects.all())
-    entry_list[0].titleF = "David"
-    print(entry_list[0].titleF)
-    
-    Feed.objects.all()[0].titleF = "David"
-    
-    print(Feed.objects.all()[0].titleF)
-    
-    
-        
-    for updateWeatherDATA in feeds_database:
-        citylocation =updateWeatherDATA.locationF
-        #print(citylocation)
-        rU =requests.get(url.format(citylocation)).json()
-        #print(rU['main']['temp'])
-        updateWeatherDATA.weatherF = rU['main']['temp']
-        updateWeatherDATA.save(update_fields=['weatherF'])
-        updateWeatherDATA.weatherdescriptionF =  r['weather'][0]['description']
-        updateWeatherDATA.save(update_fields=['weatherdescriptionF'])
-        updateWeatherDATA.weatericonF = r['weather'][0]['icon']
-        updateWeatherDATA.save(update_fields=['weatericonF'])
-        
     
     
     if request.method == 'POST':
@@ -118,8 +63,7 @@ def index(request):
             #pictureLink = request.POST['pictureLink'],
             )
         new_feed.save()
-    
-        
+    weaterforcast(feeds_database,url)
     
     return render(request, 'feeds/index.html', {'feeds_database': feeds_database, 'context': context})
     
@@ -133,7 +77,7 @@ def developer(request):
     context= {}
     context['val'] = "TEST"
     #context['s3test'] = s3module.printOutTest('intagramclone2021')
-    #context['s3test'] = s3module.ListalltheobjectsinBucket('us-east-1','intagramclone2021')
+    context['s3test'] = s3module.ListalltheobjectsinBucket('us-east-1','intagramclone2021')
     print(context)
     
     todo = Todo.objects.all()
@@ -166,4 +110,104 @@ def timeGet():
     
     return local_time 
     
+#update weather forecast
+def weaterforcast(feeds_database,url):
+    for updateWeatherDATA in feeds_database:
+        citylocation =updateWeatherDATA.locationF
+        print(citylocation)
+        newGeoWeater =requests.get(url.format(citylocation)).json()
+        print(newGeoWeater)
+        try:
+          print(newGeoWeater['main']['temp'])
+          updateWeatherDATA.weatherF = newGeoWeater['main']['temp']
+          updateWeatherDATA.save(update_fields=['weatherF'])
+          updateWeatherDATA.weatherdescriptionF =  newGeoWeater['weather'][0]['description']
+          updateWeatherDATA.save(update_fields=['weatherdescriptionF'])
+          updateWeatherDATA.weatericonF = newGeoWeater['weather'][0]['icon']
+          updateWeatherDATA.save(update_fields=['weatericonF'])
+        except Exception as e:
+          #print(e)
+          print("ERROR")
+          errorObject = feeds_database.get(locationF=citylocation)
+          errorObject.delete()
+    return feeds_database 
     
+    
+#clean up
+    """
+    
+    
+    
+    def weaterforcast(feeds_database,url):
+    for updateWeatherDATA in feeds_database:
+        citylocation =updateWeatherDATA.locationF
+        print(citylocation)
+        newGeoWeater =requests.get(url.format(citylocation)).json()
+        iftestet = str(requests.get(url.format(citylocation)).json())
+        print(newGeoWeater)
+        if iftestet == '{"cod":"404","message":"city not found"}':
+            print("ERROR newGeoWeater")
+        else:
+            print(newGeoWeater['main']['temp'])
+            updateWeatherDATA.weatherF = newGeoWeater['main']['temp']
+            updateWeatherDATA.save(update_fields=['weatherF'])
+            updateWeatherDATA.weatherdescriptionF =  newGeoWeater['weather'][0]['description']
+            updateWeatherDATA.save(update_fields=['weatherdescriptionF'])
+            updateWeatherDATA.weatericonF = newGeoWeater['weather'][0]['icon']
+            updateWeatherDATA.save(update_fields=['weatericonF'])
+        
+    return feeds_database
+    for weaterCity in feeds_database:
+        #weatherF = feeds_database.get(titleF='telex')
+        feeds_database.weatherF.weaterCity = "Balazs"
+        print(feeds_database.weatherF)
+     
+     t = feeds_database.get(titleF='telex')
+    t.weatherF = 999  # change field
+    t.save() # this will update only
+     
+    
+   
+  
+    for each in Feed.objects.all():
+        tr = feeds_database.all()
+        tr.weatherF =333
+        tr.save()
+        #test = each.weatherF
+        #each.weatherF.save()
+        print(each.weatherF)
+       
+    for updateWeatherDATA in feeds_database:
+        updateW = feeds_database.updateWeatherDATA
+        updateW.weatherF = 22
+        updateW.save()
+        
+    myQuery = Feed.objects.all()
+    for item in myQuery:
+        item.weatherF = 55
+        item.save(update_fields=['weatherF'])
+        
+        
+    entry_list = list(Feed.objects.all())
+    entry_list[0].titleF = "David"
+    print(entry_list[0].titleF)
+    
+    Feed.objects.all()[0].titleF = "David"
+    
+    print(Feed.objects.all()[0].titleF)
+
+    
+    
+   
+    for updateWeatherDATA in feeds_database:
+        citylocation =updateWeatherDATA.locationF
+        #print(citylocation)
+        newGeoWeater =requests.get(url.format(citylocation)).json()
+        #print(newGeoWeater['main']['temp'])
+        updateWeatherDATA.weatherF = newGeoWeater['main']['temp']
+        updateWeatherDATA.save(update_fields=['weatherF'])
+        updateWeatherDATA.weatherdescriptionF =  newGeoWeater['weather'][0]['description']
+        updateWeatherDATA.save(update_fields=['weatherdescriptionF'])
+        updateWeatherDATA.weatericonF = newGeoWeater['weather'][0]['icon']
+        updateWeatherDATA.save(update_fields=['weatericonF'])
+    """
